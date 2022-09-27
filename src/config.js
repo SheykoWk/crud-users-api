e5bd97d6d6564224172b4fd8a110b850271bcaaf
SheykoWk <sheyko.wk@gmail.com>
2022-09-27 14:45:10 -0500

    add crypt util and change the env variables database

--- src/config.js
+++ src/config.js
@@ -3,4 +3,10 @@ require('dotenv').config();
 module.exports = {
     port: process.env.PORT || 9000,
     jwtSecret: process.env.JWT_SECRET || 'secret',
+    db: {
+        host: process.env.DB_HOST || 'localhost',
+        username: process.env.DB_USER || 'postgres',
+        password: process.env.DB_PASSWORD || 'root',
+        database: process.env.DB
+    }
 };
---
+++ src/utils/database.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+const config = require('../config');
+
+//? Create a connection to database
+const db = new Sequelize({
+  dialect: 'postgres',
+  host: config.db.host,
+  username: config.db.username,
+  password: config.db.password,
+  database: config.db.database,
+  logging: false,
+});
+
+module.exports = { db };
